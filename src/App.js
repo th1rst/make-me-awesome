7cd79262a05e9ab0b79be886b4148d48dc19dfd4
th1rst <marco@kochannek.com>
2020-09-06 16:37:05 +0200

    working on Authentication (moved logic to higher order component)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
-import { withFirebase } from "./components/Firebase/context";
 import Home from "./pages/Home";
 import Activity from "./pages/Activity";
 import Settings from "./pages/Settings";
@@ -13,45 +12,24 @@ import FAQ from "./pages/FAQ";
 import TermsOfService from "./pages/TermsOfService";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import FirebaseTest from "./pages/FirebaseTest";
+import AuthUserContext from "./components/Session/Context";
+import withAuthentication from "./components/Session/withAuthentication";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      authUser: null,
-    };
-  }
+const App = () => (
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/personal" component={Personal} />
+      <Route exact path="/signup" component={SignUpPage} />
+      <Route exact path="/forgot" component={ForgotPasswordPage} />
+      <Route exact path="/activity" component={Activity} />
+      <Route exact path="/settings" component={Settings} />
+      <Route exact path="/overview" component={Overview} />
+      <Route exact path="/FAQ" component={FAQ} />
+      <Route exact path="/termsofservice" component={TermsOfService} />
+      <Route exact path="/privacypolicy" component={PrivacyPolicy} />
+      <Route exact path="/firebasetest" component={FirebaseTest} />
+      <Route component={Error404} />
+    </Switch>
+);
 
-  componentDidMount() {
-    this.listener = this.props.firebase.auth.onAuthStateChanged((authUser) => {
-      authUser
-        ? this.setState({ authUser })
-        : this.setState({ authUser: null });
-    });
-  }
-
-  componentWillUnmount() {
-    this.listener();
-  }
-
-  render() {
-    return (
-      <Switch>
-        <Route exact path="/" component={Home} authUser={this.state.authUser}/>
-        <Route exact path="/personal" component={Personal} authUser={this.state.authUser}/>
-        <Route exact path="/signup" component={SignUpPage} />
-        <Route exact path="/forgot" component={ForgotPasswordPage} />
-        <Route exact path="/activity" component={Activity} authUser={this.state.authUser}/>
-        <Route exact path="/settings" component={Settings} authUser={this.state.authUser}/>
-        <Route exact path="/overview" component={Overview} authUser={this.state.authUser}/>
-        <Route exact path="/FAQ" component={FAQ} />
-        <Route exact path="/termsofservice" component={TermsOfService} />
-        <Route exact path="/privacypolicy" component={PrivacyPolicy} />
-        <Route exact path="/firebasetest" component={FirebaseTest} />
-        <Route component={Error404} />
-      </Switch>
-    );
-  }
-}
-
-export default withFirebase(App);
+export default withAuthentication(App);
