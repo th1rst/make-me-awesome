5c6821daa76e6a644c55ecbefd6830df84bedc96
th1rst <marco@kochannek.com>
2020-09-07 19:41:55 +0200

    implemented user authorization (protected routes), working

---
+++ src/components/Session/withAuthorization.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+import { compose } from "recompose";
+import { withFirebase } from "../../components/Firebase/context";
+import AuthUserContext from "./Context";
+
+const withAuthorization = (condition) => (Component) => {
+  class WithAuthorization extends React.Component {
+    componentDidMount() {
+      this.listener = this.props.firebase.auth.onAuthStateChanged(
+        (authUser) => {
+          if (!condition(authUser)) {
+            this.props.history.push("/overview");
+          }
+        }
+      );
+    }
+
+    componentWillUnmount() {
+      this.listener();
+    }
+
+    render() {
+      return (
+        <AuthUserContext.Consumer>
+          {(authUser) =>
+            condition(authUser) ? <Component {...this.props} /> : null
+          }
+        </AuthUserContext.Consumer>
+      );
+    }
+  }
+
+  return compose(withRouter, withFirebase)(WithAuthorization);
+};
+
+export default withAuthorization;
--- src/pages/Overview.js
+++ src/pages/Overview.js
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
 import Navigation from "../components/Navigation";
+import AuthUserContext from "../components/Session/Context";
+import withAuthorization from "../components/Session/withAuthorization";
 
-export default class Overview extends Component {
-
-
-  render() {
-    return (
+const Overview = () => (
+  <AuthUserContext.Consumer>
+    {(authUser) => (
       <div>
         <Navigation />
-        <h1>hello from Overview Page!</h1>
+        <h1>Account: {authUser.email}</h1>
       </div>
-    );
-  }
-}
+    )}
+  </AuthUserContext.Consumer>
+);
+
+const condition = (authUser) => !!authUser;
+
+export default withAuthorization(condition)(Overview);
