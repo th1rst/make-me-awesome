4abd43506855591f202643e9c4dba99f7b3e6a6c
th1rst <marco@kochannek.com>
2020-09-24 22:50:44 +0200

    backend logic implementation of /Overview finished

--- src/pages/Overview.js
+++ src/pages/Overview.js
@@ -11,6 +11,7 @@ class Overview extends Component {
     this.state = {
       userData: null,
       username: null,
+      activities: [],
       loading: true,
     };
   }
@@ -18,7 +19,6 @@ class Overview extends Component {
     if (this.state.userData) {
       return;
     }
-    this.setState({ loading: true });
 
     this.unsubscribe = this.props.firebase
       .user(this.props.firebase.authUser.uid)
@@ -38,44 +38,40 @@ class Overview extends Component {
 
   render() {
     return (
-      <AuthUserContext.Consumer>
-        {(authUser) => (
-          <div>
-            <Navigation />
+      <div>
+        <Navigation />
 
-            <div
-              className="w-full bg-cover bg-center shadow-xl"
-              style={{
-                height: "32rem",
-                backgroundImage: `url("https://images.unsplash.com/photo-1519834785169-98be25ec3f84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80")`,
-              }}
-            >
-              <div className="flex items-center justify-center h-full w-full bg-gray-900 bg-opacity-50">
-                <div className="text-center">
-                  <h1 className="text-white text-2xl font-semibold uppercase md:text-3xl">
-                    Welcome back,{" "}
-                    <span className="underline text-blue-400">
-                      {this.state.username}
-                    </span>
-                  </h1>
+        <div
+          className="w-full bg-cover bg-center shadow-xl"
+          style={{
+            height: "32rem",
+            backgroundImage: `url("https://images.unsplash.com/photo-1519834785169-98be25ec3f84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80")`,
+          }}
+        >
+          <div className="flex items-center justify-center h-full w-full bg-gray-900 bg-opacity-50">
+            <div className="text-center">
+              <h1 className="text-white text-2xl font-semibold uppercase md:text-3xl">
+                Welcome back,{" "}
+                <span className="underline text-blue-400">
+                  {this.state.username}
+                </span>
+              </h1>
 
-                  <NewActivityModalButton />
-                </div>
-              </div>
-              <div className="flex flex-col items-center">
-                <h1 className="font-bold text-4xl md:text-5xl max-w-xl text-gray-900">
-                  Overview
-                </h1>
-                <span className="my-4 w-24 h-1 bg-blue-400 rounded-full" />
-                <div>
-                  All Activities so far: {this.state.activities}
-                  <AllActivities />
-                </div>
-              </div>
+              <NewActivityModalButton />
             </div>
           </div>
-        )}
-      </AuthUserContext.Consumer>
+          <div className="flex flex-col items-center">
+            <h1 className="font-bold text-4xl md:text-5xl max-w-xl text-gray-900">
+              Overview
+            </h1>
+            <span className="my-4 w-24 h-1 bg-blue-400 rounded-full" />
+            <div>
+              All Activities so far: {this.state.activities}
+              <AllActivities />
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
