5b0736809af42ce840e7e3508a8744b7ddc123e5
th1rst <marco@kochannek.com>
2020-10-10 23:25:33 +0200

    finished sorting algorithm for displaying data for Apexcharts, shortened ServerResponseModal code

--- src/components/Activities/SmallBarChart.js
+++ src/components/Activities/SmallBarChart.js
@@ -31,18 +31,52 @@ class SmallBarChart extends Component {
   }
 
   componentDidMount() {
-    this.filterActivitiesByProductivity("Productive");
+    this.formatDataForChart("Productive");
   }
 
-  filterActivitiesByProductivity(productiveness) {
-    const sortedArray = [];
+  formatDataForChart(productiveness) {
+    // --- get all activities of (function parameter), i.e. "productive"
+    const sortedByProcutivenessArray = [];
 
     this.props.firestoreActivities.map((entry) =>
       entry.productiveness === productiveness
-        ? sortedArray.push(entry.name)
+        ? sortedByProcutivenessArray.push(entry)
         : null
     );
-    console.log(sortedArray)
+
+    // --- get names in list, remove uppercase letters, remove multiple entries
+    //      result: (8 * "Workout" + 3 * "workout") will result in "workout"
+    const uniqueNames = Array.from(
+      new Set(
+        sortedByProcutivenessArray.map((entry) => entry.name.toLowerCase())
+      )
+    );
+
+    // --- iterate over unique names
+    const combinedDurationsArray = [];
+    uniqueNames.forEach((uniqueName) => {
+      let durationOfUniqueName = 0; // init
+
+      //  --> for each unique name, search "sortedByProcutivenessArray" and add up all the durations for that activity
+      //      result: 10x "workout" at 60 minutes each results in 1 entry in array: { name: workout, duration: 600 }
+      sortedByProcutivenessArray.forEach((activity) => {
+        if (activity.name.toLowerCase() === uniqueName) {
+          let currentDuration = parseInt(activity.duration);
+          durationOfUniqueName += currentDuration;
+        }
+      });
+      combinedDurationsArray.push({
+        name: uniqueName,
+        duration: durationOfUniqueName,
+      });
+    });
+
+    // --- sort activities from highest to lowest duration, get the top 5 entries
+    const result = combinedDurationsArray
+      .sort((activity1, activity2) => activity1.duration < activity2.duration)
+      .slice(0, 5);
+
+    console.log(result);
   }
 
   render() {
