5cc0b16ae028a8e496bc704818942edece13ed3e
th1rst <marco@kochannek.com>
2020-11-17 05:34:53 +0100

    re-written /ChangeEmailForm and /ChangePasswordForm from class-based to functional (hooks)

--- src/components/Settings/ChangeEmailForm.js
+++ src/components/Settings/ChangeEmailForm.js
@@ -1,168 +1,124 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import ServerResponseModal from "../ServerResponseModal";
 import { Label, Input, HelperText } from "@windmill/react-ui";
 import withAuthorization from "../Session/withAuthorization";
 
-class ChangeEmailForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      authUser: JSON.parse(localStorage.getItem("authUser")),
-      newEmailInput: "",
-      currentEmailInput: "",
-      showServerResponseModal: false,
-      errorMessage: "",
-      successMessage: "",
-    };
-    this.updateEmail = this.updateEmail.bind(this);
-  }
+function ChangeEmailForm(props) {
+  const authUser = JSON.parse(localStorage.getItem("authUser"));
+  const [isEditing, setIsEditing] = useState(false);
+  const [newEmail, setNewEmail] = useState("");
+  const [currentEmail, setCurrentEmail] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const [showServerResponseModal, setShowServerResponseModal] = useState(false);
 
-  handleInput = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  updateEmail() {
-    const user = this.props.firebase.auth.currentUser;
+  const updateEmail = () => {
+    const user = props.firebase.auth.currentUser;
 
     user
-      .updateEmail(`${this.state.newEmailInput}`)
+      .updateEmail(`${newEmail}`)
       .then(() => {
-        this.setState({
-          successMessage: "Sucessfully updated your email address.",
-          showServerResponseModal: true,
-          currentEmailInput: "",
-          newEmailInput: "",
-          isEditingEmail: false,
-        });
+        setSuccessMessage("Sucessfully updated your email address.");
+        setShowServerResponseModal(true);
+        setCurrentEmail("");
+        setNewEmail("");
+        setIsEditing(false);
       })
-      .then(() =>
-        setTimeout(
-          function () {
-            this.setState({ showServerResponseModal: false });
-          }.bind(this),
-          5000
-        )
-      )
+      .then(() => setTimeout(() => setShowServerResponseModal(false), 5000))
       .catch((error) => {
-        this.setState({
-          errorMessage: error.message,
-          showServerResponseModal: true,
-        });
+        setErrorMessage(error.message);
+        setShowServerResponseModal(true);
       })
-      .then(() =>
-        setTimeout(
-          function () {
-            this.setState({ showServerResponseModal: false });
-          }.bind(this),
-          5000
-        )
-      );
-  }
+      .then(() => setTimeout(() => setShowServerResponseModal(false), 5000));
+  };
 
-  checkCurrentEmailInput(userInput) {
-    if (userInput === this.state.authUser.email) {
+  const checkCurrentEmailInput = (userInput) => {
+    if (userInput === authUser.email) {
       return true;
     }
     return false;
-  }
+  };
 
-  validateEmailFormat(email) {
+  const validateEmailFormat = (email) => {
     const regexp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return regexp.test(email);
-  }
+  };
 
-  render() {
-    const {
-      currentEmailInput,
-      newEmailInput,
-      showServerResponseModal,
-      errorMessage,
-      successMessage,
-      isEditingEmail,
-    } = this.state;
+  return (
+    <div>
+      {/* ------------- HELPER TEXT ON ACTIVE ------------- */}
 
-    return (
-      <div>
-        {/* ------------- HELPER TEXT ON ACTIVE ------------- */}
+      {isEditing ? (
+        <Label>
+          <span className="font-bold">Change your email</span>
+          <Input
+            className="mb-1 mt-1"
+            placeholder="Please enter your current email"
+            value={currentEmail}
+            onChange={(e) => setCurrentEmail(e.target.value)}
+            valid={checkCurrentEmailInput(currentEmail)}
+          />
+          {checkCurrentEmailInput(currentEmail) ? (
+            <HelperText valid={true}>
+              Your current email address is correct
+            </HelperText>
+          ) : (
+            <HelperText valid={false}>
+              This is not your current email address
+            </HelperText>
+          )}
 
-        {isEditingEmail ? (
+          <Input
+            className="mb-1 mt-1"
+            placeholder="Please enter your new email"
+            value={newEmail}
+            onChange={(e) => setNewEmail(e.target.value)}
+            valid={validateEmailFormat(newEmail)}
+          />
+          {validateEmailFormat(newEmail) ? (
+            <br />
+          ) : (
+            <HelperText valid={false}>
+              Please provide an email in the following format: john@doe.com
+            </HelperText>
+          )}
+        </Label>
+      ) : (
+        <>
+          {/* ------------- NO HELPER TEXT (INACTIVE) ------------- */}
           <Label>
             <span className="font-bold">Change your email</span>
             <Input
-              name="currentEmailInput"
               className="mb-1 mt-1"
-              placeholder="Please enter your current email"
-              value={currentEmailInput}
-              onChange={this.handleInput}
-              valid={this.checkCurrentEmailInput(currentEmailInput)}
+              placeholder="Please enter current email"
+              onClick={() => setIsEditing(true)}
             />
-            {this.checkCurrentEmailInput(currentEmailInput) ? (
-              <HelperText valid={true}>
-                Your current email address is correct
-              </HelperText>
-            ) : (
-              <HelperText valid={false}>
-                This is not your current email address
-              </HelperText>
-            )}
-
+            <br />
             <Input
-              name="newEmailInput"
               className="mb-1 mt-1"
               placeholder="Please enter your new email"
-              value={newEmailInput}
-              onChange={this.handleInput}
-              valid={this.validateEmailFormat(newEmailInput)}
+              onClick={() => setIsEditing(true)}
             />
-            {this.validateEmailFormat(newEmailInput) ? (
-              <br />
-            ) : (
-              <HelperText valid={false}>
-                Please provide an email in the following format: john@doe.com
-              </HelperText>
-            )}
+            <br />
           </Label>
-        ) : (
-          <>
-            {/* ------------- NO HELPER TEXT (INACTIVE) ------------- */}
-            <Label>
-              <span className="font-bold">Change your email</span>
-              <Input
-                className="mb-1 mt-1"
-                placeholder="Please enter current email"
-                onClick={() => this.setState({ isEditingEmail: true })}
-              />
-              <br />
-              <Input
-                className="mb-1 mt-1"
-                placeholder="Please enter your new email"
-                onClick={() => this.setState({ isEditingEmail: true })}
-              />
-              <br />
-            </Label>
-          </>
-        )}
+        </>
+      )}
 
-        <button
-          className="w-26 h-12 m-2 bg-white text-gray-800 font-bold rounded border-b-2 border-blue-500 hover:border-green-600 hover:bg-green-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center"
-          onClick={this.updateEmail}
-        >
-          <span className="mr-2">Apply</span>
-        </button>
+      <button
+        className="w-26 h-12 m-2 bg-white text-gray-800 font-bold rounded border-b-2 border-blue-500 hover:border-green-600 hover:bg-green-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center"
+        onClick={updateEmail}
+      >
+        <span className="mr-2">Apply</span>
+      </button>
 
-        {/* ------------- SERVER RESPONSE MODAL ------------- */}
-
-        {showServerResponseModal ? (
-          <ServerResponseModal
-            errorMessage={errorMessage}
-            successMessage={successMessage}
-          />
-        ) : null}
-      </div>
-    );
-  }
+      {showServerResponseModal ? (
+        <ServerResponseModal
+          errorMessage={errorMessage}
+          successMessage={successMessage}
+        />
+      ) : null}
+    </div>
+  );
 }
 
 const condition = (authUser) => !!authUser;
--- src/components/Settings/ChangePasswordForm.js
+++ src/components/Settings/ChangePasswordForm.js
@@ -1,143 +1,102 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import withAuthorization from "../Session/withAuthorization";
 import { Label, Input, HelperText } from "@windmill/react-ui";
 import ServerResponseModal from "../ServerResponseModal";
 
-class ChangePasswordForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      authUser: JSON.parse(localStorage.getItem("authUser")),
-      passwordOne: "",
-      passwordTwo: "",
-      showServerResponseModal: false,
-      errorMessage: "",
-      successMessage: "",
-    };
-  }
+function ChangePasswordForm(props) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [passwordOne, setPasswordOne] = useState("");
+  const [passwordTwo, setPasswordTwo] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const [showServerResponseModal, setShowServerResponseModal] = useState(false);
 
-  handleInput = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  changePassword = async () => {
-    await this.props.firebase
-      .doPasswordUpdate(this.state.passwordOne)
+  const changePassword = async () => {
+    await props.firebase
+      .doPasswordUpdate(passwordOne)
       .then(() => {
-        this.setState({
-          successMessage: "Sucessfully updated your password.",
-          showServerResponseModal: true,
-        });
+        setSuccessMessage("Sucessfully updated your password.");
+        setShowServerResponseModal(true);
       })
-      .then(() =>
-        setTimeout(
-          function () {
-            this.setState({ showServerResponseModal: false });
-          }.bind(this),
-          5000
-        )
-      )
+      .then(() => setTimeout(() => setShowServerResponseModal(false), 5000))
       .catch((error) => {
-        this.setState({
-          errorMessage: error.message,
-          showServerResponseModal: true,
-        });
+        setErrorMessage(error.message);
+        setShowServerResponseModal(true);
       })
-      .then(() =>
-        setTimeout(
-          function () {
-            this.setState({ showServerResponseModal: false });
-          }.bind(this),
-          5000
-        )
-      );
+      .then(() => setTimeout(() => setShowServerResponseModal(true), 5000));
   };
 
-  render() {
-    const {
-      passwordOne,
-      passwordTwo,
-      errorMessage,
-      successMessage,
-      showServerResponseModal,
-      isEditingPassword,
-    } = this.state;
+  const isInvalid = passwordOne !== passwordTwo || passwordOne === "";
 
-    const isInvalid = passwordOne !== passwordTwo || passwordOne === "";
+  return (
+    <div>
+      {/* ------------- HELPER TEXT ON ACTIVE ------------- */}
 
-    return (
-      <div>
-        {/* ------------- HELPER TEXT ON ACTIVE ------------- */}
+      {isEditing ? (
+        <Label>
+          <span className="font-bold">Change your password</span>
+          <Input
+            type="password"
+            className="mb-1 mt-2"
+            placeholder="Please set a new password"
+            value={passwordOne}
+            onChange={(e) => setPasswordOne(e.target.value)}
+            valid={!isInvalid}
+          />
+          {!isInvalid ? (
+            <HelperText valid={true}>Passwords match.</HelperText>
+          ) : (
+            <HelperText valid={false}>Passwords do not match.</HelperText>
+          )}
 
-        {isEditingPassword ? (
+          <Input
+            type="password"
+            name="passwordTwo"
+            className="mb-1 mt-1"
+            placeholder="Repeat your new password"
+            value={passwordTwo}
+            onChange={(e) => setPasswordTwo(e.target.value)}
+            valid={!isInvalid}
+          />
+        </Label>
+      ) : (
+        <>
+          {/* ------------- NO HELPER TEXT (INACTIVE) ------------- */}
           <Label>
             <span className="font-bold">Change your password</span>
             <Input
-              type="password"
-              name="passwordOne"
               className="mb-1 mt-2"
               placeholder="Please set a new password"
-              value={passwordOne}
-              onChange={this.handleInput}
-              valid={!isInvalid}
+              onClick={() => setIsEditing(true)}
             />
-            {!isInvalid ? (
-              <HelperText valid={true}>Passwords match.</HelperText>
-            ) : (
-              <HelperText valid={false}>Passwords do not match.</HelperText>
-            )}
-
+            <br />
             <Input
-              type="password"
-              name="passwordTwo"
               className="mb-1 mt-1"
               placeholder="Repeat your new password"
-              value={passwordTwo}
-              onChange={this.handleInput}
-              valid={!isInvalid}
+              onClick={() => setIsEditing(true)}
             />
+            <br />
           </Label>
-        ) : (
-          <>
-            {/* ------------- NO HELPER TEXT (INACTIVE) ------------- */}
-            <Label>
-              <span className="font-bold">Change your password</span>
-              <Input
-                className="mb-1 mt-2"
-                placeholder="Please set a new password"
-                onClick={() => this.setState({ isEditingPassword: true })}
-              />
-              <br />
-              <Input
-                className="mb-1 mt-1"
-                placeholder="Repeat your new password"
-                onClick={() => this.setState({ isEditingPassword: true })}
-              />
-              <br />
-            </Label>
-          </>
-        )}
+        </>
+      )}
 
-        <button
-          className="w-26 h-12 m-2 bg-white text-gray-800 font-bold rounded border-b-2 border-blue-500 hover:border-green-600 hover:bg-green-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center"
-          onClick={this.changePassword}
-        >
-          <span className="mr-2">Apply</span>
-        </button>
+      <button
+        className="w-26 h-12 m-2 bg-white text-gray-800 font-bold rounded border-b-2 border-blue-500 hover:border-green-600 hover:bg-green-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center"
+        onClick={changePassword}
+      >
+        <span className="mr-2">Apply</span>
+      </button>
 
-        {/* ------------- SERVER RESPONSE MODAL ------------- */}
+      {/* ------------- SERVER RESPONSE MODAL ------------- */}
 
-        {showServerResponseModal ? (
-          <ServerResponseModal
-            errorMessage={errorMessage}
-            successMessage={successMessage}
-          />
-        ) : null}
-      </div>
-    );
-  }
+      {showServerResponseModal ? (
+        <ServerResponseModal
+          errorMessage={errorMessage}
+          successMessage={successMessage}
+        />
+      ) : null}
+    </div>
+  );
 }
 
 const condition = (authUser) => !!authUser;
