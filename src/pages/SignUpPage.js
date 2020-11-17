6164f0a9cd4ed113035b5d44fde53cc3d5e74a64
th1rst <marco@kochannek.com>
2020-11-17 04:18:19 +0100

    re-written /signup from class-based to functional (hooks)

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,36 +1,29 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Link, Redirect } from "react-router-dom";
 import { withFirebase } from "../components/Firebase/context";
 
-const defaultState = {
-  username: "",
-  email: "",
-  firstPassword: "",
-  secondPassword: "",
-  error: null,
-};
+function SignUpPage(props) {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [firstPassword, setFirstPassword] = useState("");
+  const [secondPassword, setSecondPassword] = useState("");
+  const [error, setError] = useState(null);
+  const [loggedIn, setLoggedIn] = useState(false);
 
-class SignUpPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { ...defaultState };
-  }
-
-  onSubmit = (event) => {
-    const { username, email, firstPassword } = this.state;
+  const onSubmit = (event) => {
     let tempUserID = null;
 
     //create FireBASE user
-    this.props.firebase
+    props.firebase
       .doCreateUserWithEmailAndPassword(email, firstPassword)
       //await UserID from Firebase auth
       .then(async () => {
-        tempUserID = await this.props.firebase.auth.currentUser.uid;
+        tempUserID = await props.firebase.auth.currentUser.uid;
       })
 
       //create corresponding FireSTORE (database) entry with UserID
       .then(() => {
-        this.props.firebase.db
+        props.firebase.db
           .collection("users")
           .doc(`${tempUserID}`)
           .set({
@@ -39,138 +32,123 @@ class SignUpPage extends Component {
             email: email,
           })
           .catch((error) => {
-            this.setState({ error });
+            setError(error);
           });
       })
       .catch((error) => {
-        this.setState({ error });
+        setError(error);
       })
       //if resolved -> Redirect to overview via setState isLoggedIn
-      .then(() => this.setState({ isLoggedIn: true }));
+      .then(() => setLoggedIn(true));
 
     event.preventDefault();
   };
 
-  handleInput = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-  };
+  const background =
+    "https://images.unsplash.com/photo-1565902603417-1f523e0736fc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80";
 
-  render() {
-    const background =
-      "https://images.unsplash.com/photo-1565902603417-1f523e0736fc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80";
+  const isInvalid =
+    firstPassword !== secondPassword ||
+    firstPassword === "" ||
+    email === "" ||
+    username === "";
 
-    const {
-      username,
-      email,
-      firstPassword,
-      secondPassword,
-      error,
-    } = this.state;
+  return loggedIn ? (
+    <Redirect to="/overview" />
+  ) : (
+    <div
+      className="absolute"
+      style={{
+        backgroundImage: `url(${background})`,
+        width: "100vw",
+        height: "100vh",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      {console.log(props)}
+      <div className="bg-grey-lighter min-h-screen flex flex-col">
+        <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+          <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full border-2 rounded-lg border-gray-200">
+            <h3 className="mb-4 pt-4 text-2xl text-center">Sign Up</h3>
+            <form onSubmit={onSubmit}>
+              <input
+                type="text"
+                className="my-2 w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                name="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Full Name"
+              />
 
-    const isInvalid =
-      firstPassword !== secondPassword ||
-      firstPassword === "" ||
-      email === "" ||
-      username === "";
+              <input
+                type="email"
+                className="my-2 w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email"
+              />
 
-    return this.state.isLoggedIn ? (
-      <Redirect to="/overview" />
-    ) : (
-      <div
-        className="absolute"
-        style={{
-          backgroundImage: `url(${background})`,
-          width: "100vw",
-          height: "100vh",
-          backgroundRepeat: "no-repeat",
-        }}
-      >
-        <div className="bg-grey-lighter min-h-screen flex flex-col">
-          <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-            <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full border-2 rounded-lg border-gray-200">
-              <h3 className="mb-4 pt-4 text-2xl text-center">Sign Up</h3>
-              <form onSubmit={this.onSubmit}>
-                <input
-                  type="text"
-                  className="my-2 w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                  name="username"
-                  value={username}
-                  onChange={this.handleInput}
-                  placeholder="Full Name"
-                />
+              <input
+                type="password"
+                className="my-2 w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                name="firstPassword"
+                value={firstPassword}
+                onChange={(e) => setFirstPassword(e.target.value)}
+                placeholder="Password"
+              />
+              <input
+                type="password"
+                className="my-2 w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                name="secondPassword"
+                value={secondPassword}
+                onChange={(e) => setSecondPassword(e.target.value)}
+                placeholder="Confirm Password"
+              />
 
-                <input
-                  type="email"
-                  className="my-2 w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                  name="email"
-                  value={email}
-                  onChange={this.handleInput}
-                  placeholder="Email"
-                />
+              <button
+                disabled={isInvalid}
+                type="submit"
+                className="my-6 w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+              >
+                Create Account
+              </button>
+              {error ? (
+                <p className="mt-2 text-red-500">{error.message}</p>
+              ) : null}
+            </form>
 
-                <input
-                  type="password"
-                  className="my-2 w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                  name="firstPassword"
-                  value={firstPassword}
-                  onChange={this.handleInput}
-                  placeholder="Password"
-                />
-                <input
-                  type="password"
-                  className="my-2 w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                  name="secondPassword"
-                  value={secondPassword}
-                  onChange={this.handleInput}
-                  placeholder="Confirm Password"
-                />
-
-                <button
-                  disabled={isInvalid}
-                  type="submit"
-                  className="my-6 w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
-                >
-                  Create Account
-                </button>
-                {error ? (
-                  <p className="mt-2 text-red-500">{error.message}</p>
-                ) : null}
-              </form>
-
+            <div className="text-center text-sm text-grey-dark mt-4">
+              By signing up, you agree to the {"  "}
+              <Link
+                to="/termsofservice"
+                className="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
+              >
+                Terms of Service
+              </Link>
+              and {"  "}
+              <Link
+                to="/privacypolicy"
+                className="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
+              >
+                Privacy Policy
+              </Link>
               <div className="text-center text-sm text-grey-dark mt-4">
-                By signing up, you agree to the {"  "}
+                Already have an account? {"  "}
                 <Link
-                  to="/termsofservice"
+                  to="/"
                   className="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
                 >
-                  Terms of Service
+                  Log in
                 </Link>
-                and {"  "}
-                <Link
-                  to="/privacypolicy"
-                  className="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
-                >
-                  Privacy Policy
-                </Link>
-                <div className="text-center text-sm text-grey-dark mt-4">
-                  Already have an account? {"  "}
-                  <Link
-                    to="/"
-                    className="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
-                  >
-                    Log in
-                  </Link>
-                  .
-                </div>
+                .
               </div>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default withFirebase(SignUpPage);
